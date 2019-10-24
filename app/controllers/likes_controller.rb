f493467c9e9eb853c20cc7baa49942e7f51f1bd2
Avinash Sarguru <avinashsarguru@gmail.com>
2019-10-24 11:49:08 -0500

    like button race condition

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,11 +1,16 @@
 class LikesController < ApplicationController
   def create
-    like = Like.create(user_id: params[:user_id], restaurant_id: params[:restaurant_id])
+    like = Like.find_or_create_by(user_id: params[:user_id], restaurant_id: params[:restaurant_id])
     render json: like, except: [:created_at, :updated_at]
   end
 
   def index
     likes = Like.all
     render json: likes, except: [:created_at, :updated_at]
+  end
+
+  def destroy
+    thing = Like.find(params[:id]).destroy
+    render json: thing
   end
 end
